6de4dbbdbcf7aad1c7c76ca0639389102be7b7eb
Prince Egbedi <princeegbedi@gmail.com>
2023-02-17 14:43:46 -0800

    Starting to code today

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+	    for (int i = 0; i < 8; i++) {
+		            for (int j = i+1; j < 9; j++) {
+				                for (int k = j+1; k < 10; k++) {
+							                int num = i * 100 + j * 10 + k;
+									                if (num == 789) {
+												                    putchar('7');
+														                        putchar('8');
+																	                    putchar('9');
+																			                    } else {
+																						                        putchar(i + '0');
+																									                    putchar(j + '0');
+																											                        putchar(k + '0');
+																														                }
+											                putchar(',');
+													                putchar(' ');
+															            }
+						        }
+			        }
+return 0;
+}
