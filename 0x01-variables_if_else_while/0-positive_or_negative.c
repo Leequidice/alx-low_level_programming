1432de3dcca2e6683f70417c1ee8a0afa95b30f9
Prince Egbedi <princeegbedi@gmail.com>
2023-02-17 09:10:30 -0800

    Starting to code today

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,11 +18,12 @@ if (n < 0)
 	printf("%d is negative\n", n);
 }
 else if (n == 0)
-{						printf("%d is zero\n", n);
+{
+	printf("%d is zero\n", n);
 }
 else
 {
 	printf("%d is positive\n", n);
-}	
-	return (0);
+}
+return (0);
 }
