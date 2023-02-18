6647971f20bd4365b1b7af6f089651c159e6fec6
Prince Egbedi <princeegbedi@gmail.com>
2023-02-17 16:30:37 -0800

    Starting to code today

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,40 +1,30 @@
 #include <stdio.h>
-
 /**
  *  * main - entry point
  *   *
- *    * Return: always returns 0
- */
+ *    * Return: alway returns 0
+ *     *
+ *      */
 int main(void)
 {
-for (int i = 0; i < 100; i++) {
-for (int j = i; j < 100; j++) {
-int n1 = i / 10;
-int n2 = i % 10;
-int n3 = j / 10;
-int n4 = j % 10;
-	if (i == j) {
-	continue;
-}
-if (i > j) {
-int temp = i;
-	i = j;
-	j = temp;
-	n1 = i / 10;
-	n2 = i % 10;
-	n3 = j / 10;
-	n4 = j % 10;
-}
-putchar(n1 + '0');
-putchar(n2 + '0');
+int i;
+int j;
+for (i = 0; i <= 99; i++)
+{
+for (j = i + 1; j <= 99; j++)
+{
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
 putchar(' ');
-putchar(n3 + '0');
-putchar(n4 + '0');
-if (i != 99 || j != 98) {
+putchar(j / 10 + '0');
+putchar(j % 10 + '0');
+if (!(i == 98 && j == 99))
+{
 putchar(',');
 putchar(' ');
 }
 }
 }
-return 0;
+putchar('\n');
+return (0);
 }
