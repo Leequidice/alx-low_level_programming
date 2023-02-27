25b7486019fa5013dbe84540b729b1d792739c57
Prince Egbedi <princeegbedi@gmail.com>
2023-02-27 05:28:47 -0800

    Starting to code today

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,17 +1,22 @@
 #include "main.h"
-include "2-strlen.c"
+
 /**
- * print_rev - reverse a string
- *
+ * print_rev - prints a string in reverse
  * @s: string to print
+ *
+ * Return: void
  */
 void print_rev(char *s)
 {
-	int i;
+        int i, len;
 
-	for (i = _strlen(s) - 1; i >= 0; i--)
-	{
-		_putchar(*(s + i));
-	}
-	_putchar('\n');
+        len = 0;
+        while (*(s + len) != '\0')
+                len++;
+
+        for (i = len - 1; i >= 0; i--)
+                _putchar(*(s + i));
+
+        _putchar('\n');
 }
+
