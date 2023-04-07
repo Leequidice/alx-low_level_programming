0cb4441eb8ac5660ee720199106d0be2158c067d
Prince Egbedi <princeegbedi@gmail.com>
2023-04-07 12:46:22 -0700

    Starting to code today

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,20 +1,25 @@
 #include "lists.h"
+#include <stdlib.h>
+
 /**
- * pop_listint - pops a node out
- * @head: linked list address
- * Return: head node data
+ * pop_listint - returns value of head node and removes it,
+ * 0 otherwise
+ *
+ * @head: head of list
+ *
+ * Return: value of node, or 0 if list empty
  */
 int pop_listint(listint_t **head)
 {
-	int value = 0;
-	listint_t *tmp;
+	int n;
+	listint_t *next;
 
-	if (*head == NULL)
-		return (value);
-	tmp = *head;
-	value = tmp->n;
+	if (head == NULL || *head == NULL)
+		return (0);
+
+	n = (*head)->n;
+	next = (*head)->next;
 	free(*head);
-	*head = tmp->next;
-
-	return (value);
+	*head = next;
+	return (n);
 }
